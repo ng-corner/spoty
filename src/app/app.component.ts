9cce0867305438eb5f89f9df89e9a92c417730f1
shiroe66 <89994320+shiroe66@users.noreply.github.com>
2023-12-13 22:52:54 +0500

    init: empty project

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, ButtonModule],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'spoty';
+}
